3994892a38ba1493f0bbc3608864b1cb0d08752c
Mohammad Shaikh <mohammad@oshaikh.sa>
2026-07-22 05:28:37 +0300

    RWD changes;

--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -7,16 +7,16 @@ function ProjectCard({title="", paragraphs=[], src="", name="", href=""})
         return(
                 <>
                         <div className='flex my-8 justify-evenly gap-0 md:gap-8'>
-                                <div className='w-[50%] md:w-[47%]'>
+                                <div className='w-2/3'>
                                          <h1 className='text-center font-bold text-xs md:text-lg'>{title}</h1>
-                                         <hr className="w-1/2 mx-auto my-2" />
+                                         {/* <hr className="w-1/2 mx-auto my-2" /> */}
                                          <Paragraphs paragraphs={paragraphs}/>
                                 </div>
-                                <div className='w-[50%] md:w-[47%]'>
+                                <div className='w-1/3'>
                                         <a href={href} target='_blank' className='
                                                 flex justify-center align-middle flex-col w-1/2 mx-auto hover:opacity-80 transition duration-300
                                         '>
-                                                <img src={src} alt="logo" className='w-full md:w-1/2 mx-auto rounded-xl'/>
+                                                <img src={src} alt="logo" className='w-full lg:w-1/2 mx-auto rounded-xl'/>
                                                 <h1 className='text-center text-xs md:text-sm my-4'>{name}</h1>
                                         </a>
                                 </div>
