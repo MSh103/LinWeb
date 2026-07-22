9b6d89f88dc7e508b38e1757aa4aeca6dab5c6b3
Mohammad Shaikh <mohammad@oshaikh.sa>
2026-07-22 04:34:46 +0300

    Theme button icon change

--- src/components/common/ClearCard.jsx
+++ src/components/common/ClearCard.jsx
@@ -1,12 +1,12 @@
 import '../../index.css';
 
-function ClearCard({children})
+function ClearCard({children, styleName=""})
 {
         return(
                 <>
-                        <div className='
-                                block w-full md:w-[47%] mt-10 px-8 py-4
-                        '>
+                        <div className={`
+                                block w-full md:w-[47%] mt-10 px-8 py-4 ${styleName}
+                        `}>
                                 {children}
                         </div>
                 </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ function Home()
                                         <SectionTitle title={home.about.title}/>
                                         <Paragraphs paragraphs={home.about.paragraphs}/>
                                 </Container>
-                                <ClearCard>
+                                <ClearCard styleName='mt-2!'>
                                         <SectionTitle title={home.hero.title}/>
                                         <img src={home.hero.imgSrc} alt="M Sh's Profile Picture" className="
                                                 rounded-full w-1/2 md:w-5/6 mx-auto mt-5
@@ -35,7 +35,7 @@ function Home()
                                 </ClearCard>
                         </div>
                         {/* <div className='w-full overflow-hidden bg-[var(--bg)] py-4 px-8 rounded-lg mt-8 shadow-lg shadow-[var(--shadow)] [border-top:1px_solid_var(--highlight)]'> */}
-                        <Container styleName='my-8'>
+                        <Container styleName='my-10!'>
                                 <h1 className='font-bold'>{home.skills.title}</h1>
                                 <SkillsScrollContainer skills={skills}/>
                         {/* </div> */}
