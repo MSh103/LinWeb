f58e55ce2a86ca60485f050c270907d44fa67f43
Mohammad Shaikh <mohammad@oshaikh.sa>
2026-07-23 19:19:05 +0300

    Icons downloaded; Production Ready Cleanup;

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,19 @@ import '../index.css'
 
 import Container from '../components/layout/Container';
 
+import Animate from '../animations/Animate';
+
 function NotFound()
 {
         return(
                 <>
+                        <Animate delay='100ms' duration='450ms'>
                         <Container>
                                 <div className='h-[50vh] flex justify-center align-middle'>
                                         <h1 className='text-center text-5xl my-auto'>Error 404</h1>
                                 </div>
                         </Container>
+                        </Animate>
                 </>
         );
 }
